a33cc69aea9b0de72f0b7c36308430aa0939d45a
jasper2326 <jasper_jiao@hotmail.com>
2017-09-12 10:30:39 +0800

    Add files via upload

---
+++ Ch1_download.py
@@ -0,0 +1,9 @@
+# author = Jasper_Jiao@ele.me
+# -*- coding: cp936 -*-
+# coding: cp936
+
+import builtwith
+import whois
+import urllib2
+import re
+
---
+++ Ch1_warm_up.py
@@ -0,0 +1,13 @@
+# author = Jasper_Jiao@ele.me
+# -*- coding: cp936 -*-
+# coding: cp936
+
+import builtwith
+import whois
+
+a = builtwith.parse('http://example.webscraping.com')
+print a
+
+
+b = whois.whois('appspot.com')
+print b
