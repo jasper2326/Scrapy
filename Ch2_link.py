5680db9cabed927c18213b75c5ea1ad0cdb4ddec
jasper2326 <jasper_jiao@hotmail.com>
2017-09-13 19:49:21 +0800

    Add files via upload

---
+++ Ch2_link.py
@@ -0,0 +1,19 @@
+# author = Jasper_Jiao@ele.me
+# -*- coding: cp936 -*-
+# coding: cp936
+
+import re
+import Ch1_download
+from bs4 import BeautifulSoup
+
+url = 'http://example.webscraping.com/places/default/view/united-kindom-239'
+html = Ch1_download.download_3(url)
+#print re.findall('<td class="w2p_fw">(.*?)</td>', html)
+#print re.findall('<td class="w2p_fw">(.*?)</td>', html)[1]
+#print re.findall('<tr id="places_area__row"><td class="w2p_fl"><label class="readonly" for="places_area" id="places_area__label">Area:</label></td><td class="w2p_fw">(>*?)</td><td class="w2p_fc"/></tr>', html)
+print re.findall('<tr id="places_area__row">.*?<td\s*class=["\']w2p_fw["\']>(.*?)</td>', html)
+
+# Beautiful soup
+broken_html = '<ul class=country><li>Area<li>Population</ul>'
+soup = BeautifulSoup(broken_html, 'html.parser')
+
